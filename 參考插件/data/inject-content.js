42a2fb5995dab5f5e168d93edbc1b342d61157fa
fu <fu@gmail.com>
2024-12-30 10:36:23 +0800

    refactor: 重構 TimezoneChangeInject 類，簡化代碼結構，優化時區變更監聽和日期處理邏輯，並確保全局 Date 對象正確反映當前時區

--- 參考插件/data/inject-content.js
+++ 參考插件/data/inject-content.js
@@ -1,228 +1,189 @@
 class TimezoneChangeInject {
+  init() {
+    this.observeTimeZoneChange();
+    this.onSettingsChanged();
+    this.runInjectCode();
+  }
 
-    init() {
-        this.observeTimeZoneChange();
-        this.onSettingsChanged();
-        this.runInjectCode();
+  onSettingsChanged() {
+    const prefs = this.observingEl.getAttribute("data-timezone-ext-el");
+    if (!prefs) {
+      return;
+    }
+    Date.prefs = JSON.parse(prefs);
+  }
+
+  observeTimeZoneChange() {
+    const observingEl = (this.observingEl = document.querySelector("[data-timezone-ext-el]"));
+    if (!observingEl) {
+      return;
     }
 
-    onSettingsChanged() {
-        const prefs = this.observingEl.getAttribute("data-timezone-ext-el");
-        if (!prefs) { return; }
+    const observer = new MutationObserver(() => this.onSettingsChanged());
+    observer.observe(observingEl, {
+      attributes: true,
+    });
+  }
 
-        Date.prefs = JSON.parse(prefs);
-    }
+  runInjectCode() {
+    Date.prefs = Date.prefs || ["Etc/GMT", 0, new Date().getTimezoneOffset(), "GMT"];
+    try {
+      Date.prefs = window.parent.Date.prefs;
+    } catch (e) {}
 
-    observeTimeZoneChange() {
-        const observingEl = this.observingEl = document.querySelector("[data-timezone-ext-el]");
-        if (!observingEl) { return; }
-    
-        const observer = new MutationObserver(() => this.onSettingsChanged());
-    
-        observer.observe(observingEl, {
-            attributes: true,
-        });
-    }
-    
-    runInjectCode() {
-        Date.prefs = Date.prefs || [
-            'Etc/GMT', 0, new Date().getTimezoneOffset(), 'London Standard Time'
-        ];
-        try { // get preferences for subframes synchronously
-            Date.prefs = window.parent.Date.prefs;
+    const ODate = Date;
+    const {
+      getTime,
+      toString,
+      toLocaleString,
+      toLocaleDateString,
+      toLocaleTimeString,
+      getDate,
+      getDay,
+      getFullYear,
+      getHours,
+      getMinutes,
+      getMonth,
+      getSeconds,
+      setDate,
+      setFullYear,
+      setHours,
+      setMinutes,
+      setMonth,
+      setSeconds,
+    } = ODate.prototype;
+
+    const ShiftedDate = class extends ODate {
+      constructor(...args) {
+        super(...args);
+        this.shiftedDate = new ODate(getTime.apply(this) + (Date.prefs[2] - Date.prefs[1]) * 60 * 1000);
+      }
+
+      // 基本方法
+      getTime() {
+        return getTime.apply(this.shiftedDate);
+      }
+
+      valueOf() {
+        return this.shiftedDate.valueOf();
+      }
+
+      // 本地時間方法
+      toString() {
+        return this.formatTimeString(toString.apply(this.shiftedDate));
+      }
+
+      toLocaleString(...args) {
+        return toLocaleString.apply(this.shiftedDate, args);
+      }
+
+      toLocaleDateString(...args) {
+        return toLocaleDateString.apply(this.shiftedDate, args);
+      }
+
+      toLocaleTimeString(...args) {
+        return toLocaleTimeString.apply(this.shiftedDate, args);
+      }
+
+      // 獲取方法
+      getDate() {
+        return getDate.apply(this.shiftedDate);
+      }
+
+      getDay() {
+        return getDay.apply(this.shiftedDate);
+      }
+
+      getFullYear() {
+        return getFullYear.apply(this.shiftedDate);
+      }
+
+      getHours() {
+        return getHours.apply(this.shiftedDate);
+      }
+
+      getMinutes() {
+        return getMinutes.apply(this.shiftedDate);
+      }
+
+      getMonth() {
+        return getMonth.apply(this.shiftedDate);
+      }
+
+      getSeconds() {
+        return getSeconds.apply(this.shiftedDate);
+      }
+
+      // 設置方法
+      setDate(...args) {
+        const oldTime = getTime.apply(this.shiftedDate);
+        const result = setDate.apply(this.shiftedDate, args);
+        const newTime = getTime.apply(this.shiftedDate);
+        super.setTime(getTime.apply(this) + (newTime - oldTime));
+        return result;
+      }
+
+      setFullYear(...args) {
+        const oldTime = getTime.apply(this.shiftedDate);
+        const result = setFullYear.apply(this.shiftedDate, args);
+        const newTime = getTime.apply(this.shiftedDate);
+        super.setTime(getTime.apply(this) + (newTime - oldTime));
+        return result;
+      }
+
+      setHours(...args) {
+        const oldTime = getTime.apply(this.shiftedDate);
+        const result = setHours.apply(this.shiftedDate, args);
+        const newTime = getTime.apply(this.shiftedDate);
+        super.setTime(getTime.apply(this) + (newTime - oldTime));
+        return result;
+      }
+
+      setMinutes(...args) {
+        const oldTime = getTime.apply(this.shiftedDate);
+        const result = setMinutes.apply(this.shiftedDate, args);
+        const newTime = getTime.apply(this.shiftedDate);
+        super.setTime(getTime.apply(this) + (newTime - oldTime));
+        return result;
+      }
+
+      setMonth(...args) {
+        const oldTime = getTime.apply(this.shiftedDate);
+        const result = setMonth.apply(this.shiftedDate, args);
+        const newTime = getTime.apply(this.shiftedDate);
+        super.setTime(getTime.apply(this) + (newTime - oldTime));
+        return result;
+      }
+
+      setSeconds(...args) {
+        const oldTime = getTime.apply(this.shiftedDate);
+        const result = setSeconds.apply(this.shiftedDate, args);
+        const newTime = getTime.apply(this.shiftedDate);
+        super.setTime(getTime.apply(this) + (newTime - oldTime));
+        return result;
+      }
+
+      getTimezoneOffset() {
+        return Date.prefs[1];
+      }
+
+      formatTimeString(str) {
+        const timezoneName = Date.prefs[3] || "GMT";
+        if (str.includes("(")) {
+          str = str.split("(")[0].trim() + " (" + timezoneName + ")";
         }
-        catch (e) { }
-    
-        const ODateTimeFormat = Intl.DateTimeFormat;
-        globalThis.Intl.DateTimeFormat = function (locales, options = {}) {
-            Object.assign(options, {
-                timeZone: Date.prefs[0]
-            });
-            return ODateTimeFormat(locales, options);
-        };
-        globalThis.Intl.DateTimeFormat.prototype = Object.create(ODateTimeFormat.prototype);
-        globalThis.Intl.DateTimeFormat.supportedLocalesOf = ODateTimeFormat.supportedLocalesOf;
-    
-        const clean = str => {
-            const toGMT = offset => {
-                const z = n => (n < 10 ? '0' : '') + n;
-                const sign = offset <= 0 ? '+' : '-';
-                offset = Math.abs(offset);
-                return sign + z(offset / 60 | 0) + z(offset % 60);
-            };
-            str = str.replace(/([T\\(])[\\+-]\\d+/g, '$1' + toGMT(Date.prefs[1]));
-            if (str.indexOf(' (') !== -1) {
-                str = str.split(' (')[0] + ' (' + Date.prefs[3] + ')';
-            }
-            return str;
-        }
-    
-        const ODate = Date;
-        const {
-            getTime, getDate, getDay, getFullYear, getHours, getMilliseconds, getMinutes, getMonth, getSeconds, getYear,
-            toDateString, toLocaleString, toString, toTimeString, toLocaleTimeString, toLocaleDateString,
-            setYear, setHours, setTime, setFullYear, setMilliseconds, setMinutes, setMonth, setSeconds, setDate,
-            setUTCDate, setUTCFullYear, setUTCHours, setUTCMilliseconds, setUTCMinutes, setUTCMonth, setUTCSeconds
-        } = ODate.prototype;
-        const ShiftedDate = class extends ODate {
-            constructor(...args) {
-                super(...args);
-                this.nd = new ODate(
-                    getTime.apply(this) + (Date.prefs[2] - Date.prefs[1]) * 60 * 1000
-                );
-            }
-            // get
-            toLocaleString(...args) {
-                return toLocaleString.apply(this.nd, args);
-            }
-            toLocaleTimeString(...args) {
-                return toLocaleTimeString.apply(this.nd, args);
-            }
-            toLocaleDateString(...args) {
-                return toLocaleDateString.apply(this.nd, args);
-            }
-            toDateString(...args) {
-                return toDateString.apply(this.nd, args);
-            }
-            getDate(...args) {
-                return getDate.apply(this.nd, args);
-            }
-            getDay(...args) {
-                return getDay.apply(this.nd, args);
-            }
-            getFullYear(...args) {
-                return getFullYear.apply(this.nd, args);
-            }
-            getHours(...args) {
-                return getHours.apply(this.nd, args);
-            }
-            getMilliseconds(...args) {
-                return getMilliseconds.apply(this.nd, args);
-            }
-            getMinutes(...args) {
-                return getMinutes.apply(this.nd, args);
-            }
-            getMonth(...args) {
-                return getMonth.apply(this.nd, args);
-            }
-            getSeconds(...args) {
-                return getSeconds.apply(this.nd, args);
-            }
-            getYear(...args) {
-                return getYear.apply(this.nd, args);
-            }
-            // set
-            setHours(...args) {
-                const a = getTime.call(this.nd);
-                const b = setHours.apply(this.nd, args);
-                setTime.call(this, getTime.call(this) + b - a);
-                return b;
-            }
-            setFullYear(...args) {
-                const a = getTime.call(this.nd);
-                const b = setFullYear.apply(this.nd, args);
-                setTime.call(this, getTime.call(this) + b - a);
-                return b;
-            }
-            setMilliseconds(...args) {
-                const a = getTime.call(this.nd);
-                const b = setMilliseconds.apply(this.nd, args);
-                setTime.call(this, getTime.call(this) + b - a);
-                return b;
-            }
-            setMinutes(...args) {
-                const a = getTime.call(this.nd);
-                const b = setMinutes.apply(this.nd, args);
-                setTime.call(this, getTime.call(this) + b - a);
-                return b;
-            }
-            setMonth(...args) {
-                const a = getTime.call(this.nd);
-                const b = setMonth.apply(this.nd, args);
-                setTime.call(this, getTime.call(this) + b - a);
-                return b;
-            }
-            setSeconds(...args) {
-                const a = getTime.call(this.nd);
-                const b = setSeconds.apply(this.nd, args);
-                setTime.call(this, getTime.call(this) + b - a);
-                return b;
-            }
-            setDate(...args) {
-                const a = getTime.call(this.nd);
-                const b = setDate.apply(this.nd, args);
-                setTime.call(this, getTime.call(this) + b - a);
-                return b;
-            }
-            setYear(...args) {
-                const a = getTime.call(this.nd);
-                const b = setYear.apply(this.nd, args);
-                setTime.call(this, getTime.call(this) + b - a);
-                return b;
-            }
-            setTime(...args) {
-                const a = getTime.call(this);
-                const b = setTime.apply(this, args);
-                setTime.call(this.nd, getTime.call(this.nd) + b - a);
-                return b;
-            }
-            setUTCDate(...args) {
-                const a = getTime.call(this);
-                const b = setUTCDate.apply(this, args);
-                setTime.call(this.nd, getTime.call(this.nd) + b - a);
-                return b;
-            }
-            setUTCFullYear(...args) {
-                const a = getTime.call(this);
-                const b = setUTCFullYear.apply(this, args);
-                setTime.call(this.nd, getTime.call(this.nd) + b - a);
-                return b;
-            }
-            setUTCHours(...args) {
-                const a = getTime.call(this);
-                const b = setUTCHours.apply(this, args);
-                setTime.call(this.nd, getTime.call(this.nd) + b - a);
-                return b;
-            }
-            setUTCMilliseconds(...args) {
-                const a = getTime.call(this);
-                const b = setUTCMilliseconds.apply(this, args);
-                setTime.call(this.nd, getTime.call(this.nd) + b - a);
-                return b;
-            }
-            setUTCMinutes(...args) {
-                const a = getTime.call(this);
-                const b = setUTCMinutes.apply(this, args);
-                setTime.call(this.nd, getTime.call(this.nd) + b - a);
-                return b;
-            }
-            setUTCMonth(...args) {
-                const a = getTime.call(this);
-                const b = setUTCMonth.apply(this, args);
-                setTime.call(this.nd, getTime.call(this.nd) + b - a);
-                return b;
-            }
-            setUTCSeconds(...args) {
-                const a = getTime.call(this);
-                const b = setUTCSeconds.apply(this, args);
-                setTime.call(this.nd, getTime.call(this.nd) + b - a);
-                return b;
-            }
-            // toString
-            toString(...args) {
-                return clean(toString.apply(this.nd, args));
-            }
-            toTimeString(...args) {
-                return clean(toTimeString.apply(this.nd, args));
-            }
-            // offset
-            getTimezoneOffset() {
-                return Date.prefs[1];
-            }
-        }
-    
-        globalThis.Date = ShiftedDate;
-    }
-    
+        return str;
+      }
+    };
+
+    globalThis.Date = ShiftedDate;
+
+    const ODateTimeFormat = Intl.DateTimeFormat;
+    globalThis.Intl.DateTimeFormat = function (locales, options = {}) {
+      options.timeZone = Date.prefs[0];
+      return new ODateTimeFormat(locales, options);
+    };
+  }
 }
+
 new TimezoneChangeInject().init();
